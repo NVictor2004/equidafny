bf4e58b6b3b9bd201b7557e609f33a3ec3fc534b
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2025-11-29 11:15:36 +0000

    feat: Update script to contain more useful functions

--- examples/scalaEquivalenceBenchmarkExamples/script.sh
+++ examples/scalaEquivalenceBenchmarkExamples/script.sh
@@ -5,8 +5,10 @@
 
 # sed -i 's/: int =/ returns (res: int) {/g' $1
 
-# sed -n "/method/H; /requires/{H; $!d}; /decreases/{H; $!d}; x; p" "./adjlist/AdjList.dfy"
+# sed -i 's/else \([^\{\}][^\{\}]*\)$/else { return \1; }/g' "$1"
+# sed -i '/./{H; $!d}; x; s/if \((.*)\) \([^\(\)\{\}][^\(\)\{\}]*\)\(\n.*else\)/if \1 { return \2; }\3/g' "$1"
+# sed -i '/./{H; $!d}; x; s/if \((.*)\) \([^\n\{\}][^\n\{\}]*\)\(\n.*else\)/if \1 { var result := \2; return result; }\3/g' "$1"
 # sed -n '/^method/{N;N;/^method.*\requires.*\ndecreases.*/p}' "./adjlist/AdjList.dfy"
 # sed -n '/^method/{N; /requires\|decreases/{N; /requires\|decreases/{p}}}' "./adjlist/AdjList.dfy"
 
-sed -i -f "stuff.sed" "$1"
+# sed -i -f "stuff.sed" "$1"
