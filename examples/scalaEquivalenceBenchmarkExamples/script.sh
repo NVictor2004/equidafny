a4301239f34947956957fd5ca714ba9eb0ba4817
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2025-11-29 00:42:53 +0000

    chore: Upload advanced sed code templates

--- examples/scalaEquivalenceBenchmarkExamples/script.sh
+++ examples/scalaEquivalenceBenchmarkExamples/script.sh
@@ -4,3 +4,9 @@
 # mv temp1.dfy $1
 
 # sed -i 's/: int =/ returns (res: int) {/g' $1
+
+# sed -n "/method/H; /requires/{H; $!d}; /decreases/{H; $!d}; x; p" "./adjlist/AdjList.dfy"
+# sed -n '/^method/{N;N;/^method.*\requires.*\ndecreases.*/p}' "./adjlist/AdjList.dfy"
+# sed -n '/^method/{N; /requires\|decreases/{N; /requires\|decreases/{p}}}' "./adjlist/AdjList.dfy"
+
+sed -i -f "stuff.sed" "$1"
