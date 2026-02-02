63321ef733380d5bd75148c38391fea0c917458c
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2026-02-02 16:02:43 +0000

    feat: Updated os-lib

--- project.scala
+++ project.scala
@@ -4,7 +4,7 @@
 // dependencies
 //> using dep com.github.j-mie6::parsley::5.0.0-M17
 //> using test.dep org.scalatest::scalatest::3.2.19
-//> using dep com.lihaoyi::os-lib::0.11.7
+//> using dep com.lihaoyi::os-lib::0.11.8
 
 //> using options -deprecation -unchecked -feature
 //> using options -Wimplausible-patterns -Wunused:all
