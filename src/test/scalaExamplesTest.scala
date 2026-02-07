fc0f54b7cf0e9b8fba16efd5cf75d7ba02d92a84
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2026-02-07 17:46:47 +0000

    refactor: Use parallelism to improve test performance

--- src/test/scalaExamplesTest.scala
+++ src/test/scalaExamplesTest.scala
@@ -7,32 +7,32 @@ import translation.program.translateProgram
 import formatter.program.formatProgram
 
 import os.proc
+import org.scalatest.ParallelTestExecution
 
-class ScalaExamplesTest extends AnyFlatSpec {
-  val scalaPath = os.pwd / "src" / "test" / "scalaEquivalenceBenchmarkExamples"
-  val scalaDirectories = os.list(scalaPath).filter(_.baseName != "incomplete")
-  val scalaExamples = scalaDirectories.flatMap(os.list(_))
+private val scalaPath = os.pwd / "src" / "test" / "scalaEquivalenceBenchmarkExamples"
+private val outputPath = os.pwd / "src" / "test" / "out"
+private val scalaDirectories = os.list(scalaPath).filter(_.baseName != "incomplete")
+private val scalaExamples = scalaDirectories.flatMap(os.list(_))
 
-  for (example <- scalaExamples) {
-    val output = program.parse(os.read(example))
+class ScalaExamplesTest extends AnyFlatSpec with ParallelTestExecution {
+  scalaExamples.foreach(example =>
+    example.last should "be parsed and formatted correctly" in {
+      val output = program.parse(os.read(example))
 
-    example.baseName should "be parsed correctly" in {
       output match {
         case Failure(msg) => fail(msg)
         case _            =>
       }
-    }
 
-    val translatedOutput = translateProgram(output.get)
-    val outputPath = os.pwd / "src" / "test" / "out" / (example.baseName + ".dfy")
-    formatProgram(translatedOutput, outputPath.toString)
+      val translatedOutput = translateProgram(output.get)
+      val outputFilePath = outputPath / example.last
+      formatProgram(translatedOutput, outputFilePath.toString)
 
-    it should "have correct syntax" in {
-      val result = proc("dafny", "resolve", "--allow-warnings", outputPath.toString).call()
+      val result = proc("dafny", "resolve", "--allow-warnings", outputFilePath.toString).call()
       result.exitCode match {
         case 0 =>
         case code => fail(s"Dafny verification failed with exitcode $code")
       }
     }
-  }
+  )
 }
