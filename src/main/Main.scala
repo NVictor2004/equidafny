de55b4ac4b8f176724dd90286f976df076049b2e
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2026-05-02 13:58:32 +0100

    feat: Written main function

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -1,9 +1,24 @@
-import scala.sys.process._
+import parsers.program.program
+import translation.program.translateProgram
+import formatter.program.formatProgram
+import equivalence.program.programEquivalence
 
+import ujson.*
+
+private val outputPath = os.pwd / "src" / "test" / "output"
+private val dafnyPath =
+  os.pwd / "examples" / "scalaEquivalenceBenchmarkExamples" / "noHelp"
+
+// TODO: Parse with Dafny parser first
 @main
-def main(file: String): Unit = {
-  println("Processing file: " + file)
+def main(jsonFile: String): Unit = {
+  val config = ujson.read(os.read(os.pwd / os.RelPath(jsonFile)))
+  val scalaFile = dafnyPath / config("file").str
 
-  val command = s"dafny $file"
-  print(command.!!)
+  val parsedOutput = program.parse(os.read(scalaFile)).get
+  val translatedOutput = translateProgram(parsedOutput, config)
+  val equivalenceOutput = programEquivalence(translatedOutput)
+
+  val outputFilePath = outputPath / scalaFile.last
+  formatProgram(equivalenceOutput, outputFilePath.toString)
 }
