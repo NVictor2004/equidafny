28adeb8f65ebd9a19166c0e367c58a3ce36f6873
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2026-05-02 14:50:49 +0100

    feat: Created flame graph of system

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -4,21 +4,25 @@ import formatter.program.formatProgram
 import equivalence.program.programEquivalence
 
 import ujson.*
-
+private val jsonPath =
+  os.pwd / "src" / "test" / "scalaEquivalenceBenchmarkExamples"
 private val outputPath = os.pwd / "src" / "test" / "output"
 private val dafnyPath =
   os.pwd / "examples" / "scalaEquivalenceBenchmarkExamples" / "noHelp"
+private val scalaExamples = os.walk(jsonPath).filter(os.isFile(_))
 
 // TODO: Parse with Dafny parser first
 @main
-def main(jsonFile: String): Unit = {
-  val config = ujson.read(os.read(os.pwd / os.RelPath(jsonFile)))
-  val scalaFile = dafnyPath / config("file").str
+def main(): Unit = {
+  scalaExamples.foreach(jsonExample => {
+    val config = ujson.read(os.read(jsonExample))
+    val scalaFile = dafnyPath / config("file").str
 
-  val parsedOutput = program.parse(os.read(scalaFile)).get
-  val translatedOutput = translateProgram(parsedOutput, config)
-  val equivalenceOutput = programEquivalence(translatedOutput)
+    val parsedOutput = program.parse(os.read(scalaFile)).get
+    val translatedOutput = translateProgram(parsedOutput, config)
+    val equivalenceOutput = programEquivalence(translatedOutput)
 
-  val outputFilePath = outputPath / scalaFile.last
-  formatProgram(equivalenceOutput, outputFilePath.toString)
+    val outputFilePath = outputPath / scalaFile.last
+    formatProgram(equivalenceOutput, outputFilePath.toString)
+  })
 }
