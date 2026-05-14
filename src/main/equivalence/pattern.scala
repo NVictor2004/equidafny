0dc05747e8a40641955123ddbbb04ec8d1499fde
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2026-05-14 16:29:16 +0100

    refactor: Remove unused functions

--- src/main/equivalence/pattern.scala
+++ src/main/equivalence/pattern.scala
@@ -2,23 +2,6 @@ package equivalence.pattern
 
 import translation.structure.* 
 
-def getIdentsFromPattern(pattern: Pattern): List[String] = pattern match {
-    case UnNamed => Nil
-    case Constant(_) => Nil
-    case Basic(name, values) => name :: values.flatMap(getIdentsFromPattern)
-    case PatternTuple(elements) => elements.flatMap(getIdentsFromPattern)
-}
-
-def containsUnNamed(pattern: Pattern): Boolean = pattern match {
-    case UnNamed => true
-    case Constant(_) => false
-    case Basic(_, values) => listContainsUnNamed(values)
-    case PatternTuple(elements) => listContainsUnNamed(elements)
-}
-
-def listContainsUnNamed(patterns: List[Pattern]): Boolean = 
-    patterns.exists(containsUnNamed)
-
 def mergePattern(modelPattern: Pattern, candPattern: Pattern): Boolean = (modelPattern, candPattern) match {
     case (_, UnNamed) => true
     case (UnNamed, _) => true
