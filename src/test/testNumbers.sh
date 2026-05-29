1f13644c454681e87202dfbee865b42e80d32339
Nathaniel Victor <nathaniel.victor23@imperial.ac.uk>
2026-05-29 18:47:58 +0100

    feat: Created script to generate numbers of tests in each class

--- src/test/testNumbers.sh
+++ src/test/testNumbers.sh
@@ -1,4 +1,32 @@
-# cand copying
-# equiv assumption
-# var inlining
-# match flattening
+
+types=$(find ./json/ -mindepth 1 -maxdepth 1 -type d ! -name "counterExamples")
+
+classNames=("automatic" "helperequivalence" "auxiliarylemmas" "induction" "termination" "higherorder" "assumption" "candcopy" "matchflattening" "varinlining" "types")
+nClasses=${#classNames[@]}
+
+for type in $types; do
+    echo "$type"
+    counts=()
+    for ((i=0; i<nClasses; i++)); do
+        counts+=(0)
+    done
+    dirs=$(find $type -mindepth 1 -maxdepth 1 -type d)
+    for dir in $dirs; do
+        dirname=$(basename "$dir")
+        dirname="${dirname,,}"
+        nfiles=$(find $dir -type f | wc -l)
+        while [[ "$dirname" != "" ]]; do
+            for ((i=0; i<nClasses; i++)); do
+                className="${classNames[i]}"
+                if [[ "$dirname" =~ ^"$className"(.*) ]]; then
+                    dirname="${BASH_REMATCH[1]}"
+                    (( counts[i]+=$nfiles ))
+                    continue 2
+                fi
+            done
+            echo "The rest of $dirname could not be matched"
+            break
+        done
+    done
+    echo "${counts[@]}"
+done
